eac30a526ccb731c6c31baa472bf893aa08c21db
pjhl <pjhl.coder@gmail.com>
2019-01-16 22:46:12 +0200

    Fixed the eslint warning

--- src/components/HighOrderComponentsExample.js
+++ src/components/HighOrderComponentsExample.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+// eslint-disable-next-line jsx-a11y/anchor-has-content
 const Link = (props) => (<a {...props} className='hoc-link' />)
 
 export default class HighOrderComponentsExample extends Component {
