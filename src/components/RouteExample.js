e6cd6275bbd383cbb6b9283b86447671c0f4bb34
pjhl <pjhl.coder@gmail.com>
2019-01-17 02:33:58 +0200

    Updated the route example: switch

--- src/components/RouteExample.js
+++ src/components/RouteExample.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const Index = () => <h2>Home</h2>
 const About = () => <h2>About</h2>
@@ -16,9 +16,11 @@ export default class RouteExample extends Component {
             <Link to='/users'>Users</Link>
           </nav>
           <div>
-            <Route path='/' exact component={Index} />
-            <Route path='/about' component={About} />
-            <Route path='/users' component={Users} />
+            <Switch>
+              <Route path='/' exact component={Index} />
+              <Route path='/about' component={About} />
+              <Route path='/users' component={Users} />
+            </Switch>
           </div>
         </Fragment>
       </Router>
