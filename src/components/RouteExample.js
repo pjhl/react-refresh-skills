7d3da0639082e835fe396fe187a6459fdee315d9
pjhl <pjhl.coder@gmail.com>
2019-01-17 02:49:34 +0200

    Updated the route example: nav links, params and error

--- src/components/RouteExample.js
+++ src/components/RouteExample.js
@@ -1,9 +1,17 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom'
 
 const Index = () => <h2>Home</h2>
 const About = () => <h2>About</h2>
 const Users = () => <h2>Users</h2>
+const User = ({ match: { params } }) => <h2>User #{params.id}</h2>
+const Error = () => <h2>404 Not Found</h2>
 
 export default class RouteExample extends Component {
   render () {
@@ -13,13 +21,22 @@ export default class RouteExample extends Component {
           <nav>
             <Link to='/'>Home</Link>{' '}
             <Link to='/about'>About</Link>{' '}
-            <Link to='/users'>Users</Link>
+            <Link to='/users'>Users</Link>{' '}
+            <Link to='/users/45'>User #45</Link>{' '}
+            <Link to='/some-unexistance/url'>404</Link>
+          </nav>
+          <nav>
+            <NavLink to='/' exact>Home</NavLink>{' '}
+            <NavLink to='/about'>About</NavLink>{' '}
+            <NavLink to='/users'>Users</NavLink>{' '}
           </nav>
           <div>
             <Switch>
               <Route path='/' exact component={Index} />
               <Route path='/about' component={About} />
-              <Route path='/users' component={Users} />
+              <Route path='/users' exact component={Users} />
+              <Route path='/users/:id' component={User} />
+              <Route path='*' component={Error} />
             </Switch>
           </div>
         </Fragment>
