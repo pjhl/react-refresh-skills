f6d7dff530f4824042a13f0532b12cf030521af1
Pjhl <seregal2009@yandex.ru>
2019-01-30 13:50:13 +0200

    Installed and configured the redux

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -4,16 +4,20 @@ import Tasks from './containers/Tasks'
 import Footer from './components/Footer'
 import TodoInput from './components/TodoInput'
 import './todo.css'
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default class TodoApp extends Component {
   render () {
     return (
-      <div>
-        <Title name='ToDo list' />
-        <TodoInput onChange={() => { console.log('Input changed') }} />
-        <Tasks />
-        <Footer records={[]} />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Title name='ToDo list' />
+          <TodoInput onChange={() => { console.log('Input changed') }} />
+          <Tasks />
+          <Footer records={[]} />
+        </div>
+      </Provider>
     )
   }
 }
